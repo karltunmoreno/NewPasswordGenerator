247ac26e332282172623c74a1d14a488fd13fbf9
unknown <karltunmoreno@gmail.com>
2025-02-11 16:20:10 -0500

    push

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using PasswordGenerator;
+
+var pwd = new Password();
+var password = pwd.Next();
+
+Console.WriteLine(password);
+
+
+
